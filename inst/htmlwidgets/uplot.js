c18b20ec38d9afd174ade7aa7b994a206f1db783
Tomáš Martinovič <martinovic@eduroam-231-68.vsb.cz>
2021-08-16 14:06:59 +0200

    updated uplot to 1.6.15 from 1.6.9 Jan Swiatkowski added possibility to declare plugins in R as string and then use them as function in JS added touch zoom plugin

--- inst/htmlwidgets/uplot.js
+++ inst/htmlwidgets/uplot.js
@@ -9,20 +9,30 @@ HTMLWidgets.widget({
      var elementId = el.id;
      var container = document.getElementById(elementId);
    //  var uplot = new uPlot(opts, null, container);
-     var uplot = null;
+     var u = null;
     return {
 
       renderValue: function(x) {
         // set width and height to match the container
         x.opts.width = width - 20
         x.opts.height = height - 80
-
+        // function example(){console.log('example')}
+        let plugs = null
+        if(x.opts.plugins) {
+          const supportedPlugins = [touchZoomPlugin]
+          plugs = x.opts.plugins.map((p) => {
+            const f = supportedPlugins.find((sp) => sp.name === p)
+            if(f) {return f()}
+            else {return null}
+          })
+          .filter((p)=>p !== null)
+        }
         // clear plot if already exists
-        if(uplot) {
-          uplot.destroy()
+        if(u) {
+          u.destroy()
         }
 
-        uplot = new uPlot(x.opts, x.data, container);
+        u = new uPlot({...x.opts,plugins:plugs}, x.data, container);
       },
 
       resize: function(width, height) {
@@ -32,7 +42,7 @@ HTMLWidgets.widget({
     					height: height - 80,
     				}
     			}
-        uplot.setSize(getSize());
+        u.setSize(getSize());
       }
 
     };
